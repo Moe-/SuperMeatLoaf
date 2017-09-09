e77f20ec1bde18ecbcca5a64c580c3ef56c53057
Moe <moe@diccuric.org>
2017-09-09 13:13:38 +0200

    New demo level

---
+++ maps/Unbenannt.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tilemap" tilewidth="64" tileheight="64" tilecount="32" columns="4">
+ <image source="../gfx/tilemap.png" width="256" height="512"/>
+</tileset>
